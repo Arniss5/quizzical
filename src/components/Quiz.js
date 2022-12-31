2fee69ab0e7fe035aa22232ce2abc8c799d488a9
Your NameArniss5 <kat.dobr@gmail.com>
2022-12-31 12:31:40 +0000

    create question objects

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,9 +3,9 @@ import React from "react"
 
 export default function Quiz(props) {
 
-    const [quizData, setquizData] = React.useState([])
-
-    //useRef() keeps track of a boolean
+    const [quizData, setQuizData] = React.useState([])
+    
+    // useRef() keeps track of a boolean
     const dataFetchedRef = React.useRef(false);
 
     React.useEffect(() => {
@@ -15,10 +15,31 @@ export default function Quiz(props) {
         
         fetch(`https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple`)
             .then(res => res.json())
-            .then(data => setquizData(data))
+            .then(data => {
+                // getQuizItems(data)
+                // setQuizData(getQuizItems(data.results))
+                
+                console.log(getQuizItems(data.results))})
+                
       }, [])
 
-
+      
+      function getQuizItems(data) {
+        let questionArr = []
+        
+        data.forEach(item => {
+            const answers = item.incorrect_answers.concat(item.correct_answer) 
+            const shuffledAnswers = answers.sort(() => Math.random() - 0.5)
+            questionArr.push({
+                question: item.question,
+                answers: shuffledAnswers,
+                correct_answer: item.correct_answer
+            })
+        })
+        return questionArr
+      }
+      
+    
     return (
         <div className="container">
             <Question />
